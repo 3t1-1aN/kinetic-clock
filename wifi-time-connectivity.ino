2952bb1ba754dc5b08d7bfffac268bf5e06343b1
3t1-1aN <147291550+3t1-1aN@users.noreply.github.com>
2025-09-26 14:59:57 -0700

    NTP/RTC connectivity

--- wifi-time-connectivity.ino
+++ wifi-time-connectivity.ino
@@ -1,51 +1,88 @@
-#include <WiFiS3.h>
-#include <WiFiUdp.h>
-#include <NTPClient.h>
+// Include the RTC library
 #include "RTC.h"
 
-char ssid[] = "YOUR_SSID";
-char pass[] = "YOUR_PASSWORD";
+//Include the NTP library
+#include <NTPClient.h>
 
-WiFiUDP ntpUDP;
-NTPClient timeClient(ntpUDP, "pool.ntp.org", 0, 60000); // sync every 60s
+#include <WiFiS3.h>
+#include <WiFiUdp.h>
+#include "arduino_secrets.h" 
 
-void setup() {
-  Serial.begin(115200);
 
-  // connect to WiFi
-  WiFi.begin(ssid, pass);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
+char ssid[] = SECRET_SSID; 
+char pass[] = SECRET_PASS;
+
+int wifiStatus = WL_IDLE_STATUS;
+WiFiUDP Udp; // A UDP instance to let us send and receive packets over UDP
+NTPClient timeClient(Udp);
+
+void printWifiStatus() {
+  // print the SSID of the network attached to:
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  // print the board's IP address:
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+
+  // print the received signal strength:
+  long rssi = WiFi.RSSI();
+  Serial.print("signal strength (RSSI):");
+  Serial.print(rssi);
+  Serial.println(" dBm");
+}
+
+void connectToWiFi(){
+  // check for the WiFi module:
+  if (WiFi.status() == WL_NO_MODULE) {
+    Serial.println("Communication with WiFi module failed!");
+    while (true);
   }
 
-  // start NTP
+  String fv = WiFi.firmwareVersion();
+  if (fv < WIFI_FIRMWARE_LATEST_VERSION) {
+    Serial.println("Please upgrade the firmware");
+  }
+
+  // attempt to connect to WiFi network:
+  while (wifiStatus != WL_CONNECTED) {
+    Serial.print("Attempting to connect to SSID: ");
+    Serial.println(ssid);
+    // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
+    wifiStatus = WiFi.begin(ssid, pass);
+
+    // wait 10 seconds for connection:
+    delay(10000);
+  }
+
+  Serial.println("Connected to WiFi");
+  printWifiStatus();
+}
+
+void setup(){
+  Serial.begin(9600);
+  while (!Serial);
+
+  connectToWiFi();
+  RTC.begin();
+  Serial.println("\nStarting connection to server...");
   timeClient.begin();
+  timeClient.update();
 
-  // sync RTC once at startup
-  updateRTC();
-}
+  // Get the current date and time from an NTP server and convert
+  // it to UTC +2 by passing the time zone offset in hours.
+  auto timeZoneOffsetHours = 2;
+  auto unixTime = timeClient.getEpochTime() + (timeZoneOffsetHours * 3600);
+  Serial.print("Unix time = ");
+  Serial.println(unixTime);
+  RTCTime timeToSet = RTCTime(unixTime);
+  RTC.setTime(timeToSet);
 
-void loop() {
-  static unsigned long lastSync = 0;
-
-  if (millis() - lastSync > 60000) { // every minute
-    updateRTC();
-    lastSync = millis();
-  }
-
-  // read RTC for "always-on" time
+  // Retrieve the date and time from the RTC and print them
   RTCTime currentTime;
-  RTC.getTime(currentTime);
-  Serial.println(currentTime.toString());
-  delay(1000);
+  RTC.getTime(currentTime); 
+  Serial.println("The RTC was just set to: " + String(currentTime));
 }
 
-void updateRTC() {
-  if (timeClient.update()) {
-    unsigned long epoch = timeClient.getEpochTime();
-    RTCTime newTime(epoch);
-    RTC.setTime(newTime);
-    Serial.println("RTC synced!");
-  }
-}
+void loop(){}
