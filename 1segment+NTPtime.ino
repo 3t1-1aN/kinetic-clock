a9f50dfa1b833552f9a96c368b04edddbb0a73b1
3t1-1aN <147291550+3t1-1aN@users.noreply.github.com>
2025-10-15 09:01:55 -0700

    Update 1segment+NTPtime.ino

--- 1segment+NTPtime.ino
+++ 1segment+NTPtime.ino
@@ -6,109 +6,99 @@
 
 const int SERVO_PINS[7] = {6, 7, 8, 9, 10, 11, 12};
 Servo servos[7];
+
 const int DIGIT_TO_SEGMENT[10][7] = {
-  {1, 1, 1, 1, 1, 1, 0}, //0
-  {0, 1, 1, 0, 0, 0, 0}, //1
-  {1, 1, 0, 1, 1, 0, 1}, //2
-  {1, 1, 1, 1, 0, 0, 1}, //3
-  {0, 1, 1, 0, 0, 1, 1}, //4
-  {1, 0, 1, 1, 0, 1, 1}, //5
-  {1, 0, 1, 1, 1, 1, 1}, //6
-  {1, 1, 1, 0, 0, 0, 0}, //7
-  {1, 1, 1, 1, 1, 1, 1}, //8
-  {1, 1, 1, 1, 0, 1, 1}  //9
-} ;
+  {1, 1, 1, 1, 1, 1, 0},
+  {0, 1, 1, 0, 0, 0, 0},
+  {1, 1, 0, 1, 1, 0, 1},
+  {1, 1, 1, 1, 0, 0, 1},
+  {0, 1, 1, 0, 0, 1, 1},
+  {1, 0, 1, 1, 0, 1, 1},
+  {1, 0, 1, 1, 1, 1, 1},
+  {1, 1, 1, 0, 0, 0, 0},
+  {1, 1, 1, 1, 1, 1, 1},
+  {1, 1, 1, 1, 0, 1, 1}
+};
 
-int SERVO_OFF = 160;
-int SERVO_ON = 40;
+const int SERVO_OFF = 160;
+const int SERVO_ON = 40;
 
-char ssid[] = "Mechatronics W183"; 
+char ssid[] = "Mechatronics W183";
 char pass[] = "Mechatronics183";
 
 int wifiStatus = WL_IDLE_STATUS;
-WiFiUDP Udp; // A UDP instance to let us send and receive packets over UDP
+WiFiUDP Udp;
 NTPClient timeClient(Udp);
 
-void connectToWiFi(){
+unsigned long lastSync = 0;
+const unsigned long syncInterval = 3600000; // 1 hour
+
+void updateRTC() {
+  timeClient.update();
+  unsigned long unixTime = timeClient.getEpochTime();
+  RTCTime newTime = RTCTime(unixTime);
+  RTC.setTime(newTime);
+  Serial.println("RTC updated from NTP");
+}
+
+void connectToWiFi() {
   String fv = WiFi.firmwareVersion();
-  if (fv < WIFI_FIRMWARE_LATEST_VERSION) {
+  if (fv != WIFI_FIRMWARE_LATEST_VERSION) {
     Serial.println("Please upgrade the firmware");
   }
 
-  // attempt to connect to WiFi network:
   while (wifiStatus != WL_CONNECTED) {
-    Serial.print("Attempting to connect to SSID: ");
+    Serial.print("Connecting to SSID: ");
     Serial.println(ssid);
-    // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
     wifiStatus = WiFi.begin(ssid, pass);
     delay(10000);
   }
-
   Serial.println("Connected to WiFi");
 }
 
 void setup() {
-  // put your setup code here, to run once:
-/* WIFI */
   Serial.begin(9600);
   while (!Serial);
 
   connectToWiFi();
   RTC.begin();
-  Serial.println("\nStarting connection to server...");
+
+  Serial.println("Starting NTP...");
   timeClient.begin();
   timeClient.update();
-  updateRTC(); // initial sync at startup
-  lastSync = millis();
-  auto timeZoneOffsetHours = 2;
-  auto unixTime = timeClient.getEpochTime() + (timeZoneOffsetHours * 3600);
-  Serial.print("Unix time = ");
-  Serial.println(unixTime);
-  RTCTime timeToSet = RTCTime(unixTime);
-  RTC.setTime(timeToSet);
-  RTCTime currentTime;
-  RTC.getTime(currentTime); 
-  Serial.println("The RTC was just set to: " + String(currentTime));
+  updateRTC();
 
-/* SERVO MOTORS */
-  for (int i=0; i<7; i++) {
+  RTCTime currentTime;
+  RTC.getTime(currentTime);
+  Serial.print("RTC initialized.\n");
+
+  for (int i = 0; i < 7; i++) {
     servos[i].attach(SERVO_PINS[i]);
     servos[i].write(SERVO_OFF);
   }
 }
 
 void showDigits(int digit) {
-  for (int s=0; s<7; s++) {
-    int state = DIGIT_TO_SEGMENT[digit][s];
-    if (state == 1) {
-      servos[s].write(SERVO_ON);
-    } 
-    else {
-      servos[s].write(SERVO_OFF);
-    }
+  for (int s = 0; s < 7; s++) {
+    servos[s].write(DIGIT_TO_SEGMENT[digit][s] ? SERVO_ON : SERVO_OFF);
   }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-/* WIFI */
   unsigned long now = millis();
 
-  // Update RTC every 1 hour
   if (now - lastSync >= syncInterval) {
     updateRTC();
     lastSync = now;
   }
 
-  // Print current RTC time every second
   RTCTime currentTime;
   RTC.getTime(currentTime);
-  Serial.println("RTC: " + String(currentTime));
+  Serial.print("RTC Hour: ");
+  Serial.println(currentTime.getHour());
 
-  delay(1000);
-/* SERVO MOTORS */
-  for (int d=0; d<=9; d++) {
-  showDigits(d) ;
-  delay(500);
+  for (int d = 0; d <= 9; d++) {
+    showDigits(d);
+    delay(500);
   }
 }
